fb14e2830ffe4f462c6d008143e74949a50004ad
Lance Ivy <lance@cainlevy.net>
2015-01-27 13:36:32 -0800

    start with a dash of holman

--- install.sh
+++ install.sh
@@ -1,2 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+# TODO:
+# install homebrew packages on Mac
+# install .deb packages on Ubuntu
+# install sublime packages & settings
+
+DOTFILES_ROOT=$(pwd)
+
+install_dotfiles () {
+  echo 'installing dotfiles'
+
+  for src in $(find "$DOTFILES_ROOT" -name '*.symlink')
+  do
+    dst="$HOME/.$(basename "${src%.*}")"
+    ln -s "$src" "$dst"
+  done
+}
+
+install_dotfiles
